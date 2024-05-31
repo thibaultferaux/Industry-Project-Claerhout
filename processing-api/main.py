f7a8763399c9c8686b05b6370e9e11761db294e7
Thibault <thibault.feraux@hotmail.com>
2024-05-31 14:25:58 +0200

    chore: cleaned up code

--- processing-api/main.py
+++ processing-api/main.py
@@ -2,6 +2,7 @@ import math
 from fastapi import FastAPI
 import numpy as np
 import logging
+import json
 
 from azure_utils.storage_queue import get_queue_client, fetch_urls_from_queue
 from azure_utils.cosmos import update_job_results, set_job_status
@@ -13,13 +14,16 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
-smtp_server = os.environ.get('SMTP_SERVER')
-smtp_port = os.environ.get('SMTP_PORT')
-username = os.environ.get('SMTP_USERNAME')
-password = os.environ.get('SMTP_PASSWORD')
+smtp_server = os.getenv('SMTP_SERVER')
+smtp_port = os.getenv('SMTP_PORT')
+username = os.getenv('SMTP_USERNAME')
+password = os.getenv('SMTP_PASSWORD')
 
 logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -57,7 +61,7 @@ async def process_queue(job_id: str):
                 # When urls are retrieved, process the images
                 logging.info(f"Processing {len(urls)} images for job {job_id}")
                 results = await handle_image_batch(urls, container_client)
-                print("RESULTS", results)
+                logging.debug(f"Image Batch Results: {results}")
                 job = await update_job_results(job_id, results, len(urls))
                 logging.info(f"Processed {len(urls)} images for job {job_id}")
 
@@ -67,26 +71,30 @@ async def process_queue(job_id: str):
                     logging.info(f"Job {job_id} completed")
                     await set_job_status(job_id, "completed")
                     # Delete the queue
+                    logging.info(f"Deleting queue {job_id}")
                     queue_client.delete_queue()
                     # Delete the blob container
                     # container_client.delete_container()
-                    from_email = 'noreply@pebbles-health.be'
-                    to_email = 'mattis.dewintere@gmail.com'
+
+                    to_email = 'thibault.feraux@hotmail.com'
+                    logging.info(f"Sending email from {username} to {to_email}")
                     subject = 'Resultaten Roof Radar'
-                    body = job
+                    body = json.dumps(job, indent=4)
+                    logging.debug(f"Email body: {body}")
                     msg = MIMEMultipart()
-                    msg['From'] = from_email
+                    msg['From'] = username
                     msg['To'] = to_email
                     msg['Subject'] = subject
                     msg.attach(MIMEText(body, 'plain'))
+                    logging.debug(f"Email message: {msg.as_string()}")
                     try:
                         server = smtplib.SMTP(smtp_server, smtp_port)
                         server.starttls()  # Secure the connection
                         server.login(username, password)
-                        server.sendmail(from_email, to_email, msg.as_string())
-                        print("Email sent successfully")
+                        server.sendmail(username, to_email, msg.as_string())
+                        logging.info("Email sent successfully")
                     except Exception as e:
-                        print(f"Failed to send email: {e}")
+                        logging.error(f"Failed to send email: {e}")
                     return
 
         except Exception as e:
